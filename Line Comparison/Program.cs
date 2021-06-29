c3de95fd99c1672c6b4abff89ec4c13fbf475f63
AkshayKumarTN <tnak369@gmail.com>
2021-06-29 10:47:16 +0530

    [Akshay] Initial Commit

--- Line Comparison/Program.cs
+++ Line Comparison/Program.cs
@@ -6,6 +6,7 @@ namespace Line_Comparison
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Line Comparison Computation.");
             //A Length as 2 Points (x1, y1) and (x2, y2)
             // Read the values of 2 Points (x1, y1) and (x2, y2)
             Console.Write("Enter the value of x1 : ");
@@ -16,11 +17,10 @@ namespace Line_Comparison
             int x2 = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter the value of y2 : ");
             int y2 = Convert.ToInt32(Console.ReadLine());
-            double length_of_line= Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) * 1.0);
+            double length_of_line= Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
             //the Length of 2 Points (x1, y1) and (x2, y2) is given
             Console.WriteLine("The Length of 2 Points("+x1+","+y1+") and("+x2+","+y2+") is "+ length_of_line);
 
-            Console.WriteLine("Welcome to Line Comparison Computation Program on Master Branch.");
         }
     }
 }
